b084aa0ab2c889c775c4396766c35b9675d2b1cf
Damon <714348588@qq.com>
2024-01-02 14:49:39 +0800

    动态路由方法编写

--- Vue单页面应用/vue-Demo/src/router/index.js
+++ Vue单页面应用/vue-Demo/src/router/index.js
@@ -1,23 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
+const pages =import.meta.glob('../views/**/page.js',{
+  eager:true,
+  import:'default'
+})
+
+const comps=import.meta.glob('../views/**/index.vue');
+const routes= Object.entries(pages).map(([path,meta])=>{
+  const comPath=path.replace('page.js','index.vue');
+  
+  path=path.replace('../views','').replace('/page.js','')|| '/';
+  console.log(comps);
+  const name=path.split('/').filter(Boolean).join('-')||'index'
+  return {
+    path,
+    name,
+    component:comps[comPath],
+    meta
+  }
+});
+const routes1={
+  path:'/',
+  name:'index',
+  component:()=>import('../views/home/index.vue'),
+  children:routes
+};
+console.log(routes);
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes:[...routes,routes1]
 })
 
 export default router
