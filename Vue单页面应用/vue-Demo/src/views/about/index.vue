5089055d0a94bca2706c29aee66c62fe11cb8e1c
Damon <714348588@qq.com>
2024-01-18 10:07:16 +0800

    webview2与wpf通信方法

--- Vue单页面应用/vue-Demo/src/views/about/index.vue
+++ Vue单页面应用/vue-Demo/src/views/about/index.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="bg-red-400 about mt-100" >
     <h1>This is an about page</h1>
+    <button @click="click">click</button>
   </div>
 </template>
+
+<script setup>
+function click(){
+  console.log('click');
+  window.chrome.webview.postMessage('hello world');
+}
+
+window.chrome.webview.addEventListener('message', (event) => {
+  console.log(event);
+  alert(event.data);
+});
+</script>
+
 
 <style>
 /* @media (min-width: 1024px) {
