c081404a012eaf43be47059d1bee4aa2f8a8d1aa
wangwenchao <wangwenchao@pcitech.com>
2024-02-22 17:15:54 +0800

    Avalonia项目

---
+++ C#/Avalonia/AvaloniaApplication2/AvaloniaApplication2.Desktop/CefAppImpl.cs
@@ -0,0 +1,30 @@
+﻿using CefNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication2.Desktop
+{
+    internal class CefAppImpl : CefNetApplication
+    {
+        protected override void OnBeforeCommandLineProcessing(string processType, CefCommandLine commandLine)
+        {
+            base.OnBeforeCommandLineProcessing(processType, commandLine);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                commandLine.AppendSwitch("no-zygote");
+                commandLine.AppendSwitch("no-sandbox");
+            }
+        }
+        public Action<long> ScheduleMessagePumpWorkCallback { get; set; }
+
+        protected override void OnScheduleMessagePumpWork(long delayMs)
+        {
+            ScheduleMessagePumpWorkCallback(delayMs);
+        }
+    }
+}
---
+++ C#/Avalonia/AvaloniaApplication2/AvaloniaApplication2/Views/MainWindow.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using CefNet.Avalonia;
+
+namespace AvaloniaApplication2.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        //WebViewControl.WebView webView = new WebViewControl.WebView();
+        //Content = webView;
+        //webView.LoadUrl("http://dev-k8s.pcitech.com:31114//hjmos-lnc-web/#/H5Page/h5Panorama");
+        WebView webview = new() { Focusable = true };
+        Content = webview;
+
+        webview.BrowserCreated += (s, e) => webview.Navigate("http://dev-k8s.pcitech.com:31114//hjmos-lnc-web/#/H5Page/h5Panorama");
+
+        webview.DocumentTitleChanged += (s, e) => Title = e.Title;
+
+    }
+
+   
+}
