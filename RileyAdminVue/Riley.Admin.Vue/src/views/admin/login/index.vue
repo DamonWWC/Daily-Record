c28041efee29f5c2c269f7c543402bad59de9950
wangwenchao <wangwenchao@pcitech.com>
2024-04-30 11:37:43 +0800

    创建riley前端界面

---
+++ RileyAdminVue/Riley.Admin.Vue/src/views/admin/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-scrollbar>
+        <div class="login-container"></div>
+    </el-scrollbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ RileyAdminVue/Riley.Admin.Vue/vite.config.ts
@@ -0,0 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+    AutoImport({
+      imports: ['vue', 'vue-router','pinia'],
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    open: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
