c117c2b4fae20608db93d46b32c8c05e137dffc7
Damon <714348588@qq.com>
2024-05-17 10:21:34 +0800

    22

---
+++ WebApi/RileyAdmin/Riley.Admin/Core/HostApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Riley.Admin.Core
+{
+    public class HostApp
+    {
+
+        public HostApp()
+        {
+            
+        }
+
+        
+    }
+}
