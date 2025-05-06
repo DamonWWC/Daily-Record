90489e7250df47fec897950b38404f8c553e001f
wangwenchao <wangwenchao@pcitech.com>
2025-05-06 11:26:42 +0800

    Pipeline的demo

--- C#/MethodDemo/MethodDemo/AsyncLoacalSample/Program.cs
+++ C#/MethodDemo/MethodDemo/AsyncLoacalSample/Program.cs
@@ -6,20 +6,44 @@
         static async Task Main(string[] args)
         {
 
+            //await Start();
             asyncLocalValue.Value = 10;
             Console.WriteLine($"Main before async:{asyncLocalValue.Value}");
 
-            await DoAsyncWork();
+            await DoAsyncWork().ConfigureAwait(false);
             Console.WriteLine($"Main after async: {asyncLocalValue.Value}");
             Console.WriteLine("Hello, World!");
         }
         static async Task DoAsyncWork()
         {
+            
             Console.WriteLine($"DoAsyncWork before change: {asyncLocalValue.Value}");
             asyncLocalValue.Value = 20;
             Console.WriteLine($"DoAsyncWork after change: {asyncLocalValue.Value}");
-            await Task.Delay(100);
+            await Task.Delay(100).ConfigureAwait(false);
             Console.WriteLine($"DoAsyncWork after delay: {asyncLocalValue.Value}");
         }
+        //private static AsyncLocal<string> _context = new AsyncLocal<string>();
+        //public static async Task Start()
+        //{
+        //    _context.Value = "Initial Value";
+        //    Console.WriteLine($"Start: {_context.Value}"); // 输出 Initial Value
+
+        //    await StepOne();
+        //    Console.WriteLine($"After StepOne: {_context.Value}"); // 输出 Updated in StepTwo
+        //}
+
+        //private static async Task StepOne()
+        //{
+        //    _context.Value = "Updated in StepOne";
+        //    await StepTwo();
+        //}
+
+        //private static async Task StepTwo()
+        //{
+        //    await Task.Delay(100); // 模拟异步操作
+        //    Console.WriteLine($"StepTwo: {_context.Value}"); // 输出 Updated in StepOne
+        //    _context.Value = "Updated in StepTwo";
+        //}
     }
 }
---
+++ C#/MethodDemo/MethodDemo/PipelinesDemo/FileProcessor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelinesDemo
+{
+    public class FileProcessor
+    {
+        public async Task ProcessFileAsync(string filePath)
+        {
+            await using var fileStream = File.OpenRead(filePath);
+            var pipe = new Pipe();
+
+            var writing = FillPipeAsync(fileStream, pipe.Writer);
+            var reading = ReadPipeAsync(pipe.Reader);
+
+            await Task.WhenAll(reading, writing);
+            
+        }
+        public async Task FillPipeAsync(FileStream fileStream,PipeWriter writer)
+        {
+            const int minimumBufferSize = 1024;
+            try
+            {
+                while (true)
+                {
+                    Memory<byte> memory = writer.GetMemory(minimumBufferSize);
+                    try
+                    {
+                        int bytesRead = await fileStream.ReadAsync(memory);
+                        if (bytesRead == 0)
+                            break;
+                        //表示数据已经写入,读取端感知新数据的存在。
+                        writer.Advance(bytesRead);
+                    }
+                    catch(Exception ex)
+                    {
+                        break;
+                    }
+                    //FlushAsync负责将数据从管道的写入端推送到读取端。
+                    var result = await writer.FlushAsync();
+                    if (result.IsCompleted)
+                        break;
+                }
+            }
+            finally
+            {
+                //当写入完成后，表示不再写入新数据，此时管道会通知读取端处理剩余数据并结束。
+                writer.Complete();
+            }
+        }
+        public async Task ReadPipeAsync(PipeReader reader)
+        {
+            try
+            {
+                while(true)
+                {
+                    ReadResult result = await reader.ReadAsync();
+                    ReadOnlySequence<byte> buffer = result.Buffer;
+
+                    //ProcessBuffer
+
+                    reader.AdvanceTo(buffer.End);
+
+                    if (result.IsCompleted)
+                        break;
+                }
+            }
+            finally
+            {
+                reader.Complete();
+            }
+        }
+    }
+}
